9bbabb9343657b62f79a95e7023a578543f29544
NH Evan <nhevan@gmail.com>
2018-05-20 18:08:46 +0600

    run 1

--- test.php
+++ test.php
@@ -1,2 +1,13 @@
 <?php
 
+use Calcinai\PHPi\Pin\PinFunction;
+
+require "vendor/autoload.php";
+
+$board = Calcinai\PHPi\Factory::create();
+
+
+$pin = $board->getPin(4) //BCM pin number
+->setFunction(PinFunction::OUTPUT);
+
+$pin->high();
