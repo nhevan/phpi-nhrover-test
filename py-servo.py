15de4a7734843bc8a03f0345b8841e4f7c9c1294
NH Evan <nhevan@gmail.com>
2018-05-21 20:45:58 +0600

    Python script updated

--- py-servo.py
+++ py-servo.py
@@ -5,12 +5,16 @@ from time import sleep
 
 def setServoPos(servo, pos):
 	if pos==0:
+		print("Turning to mid position \n")
 		servo.mid()
 	if pos==1:
+		print("Turning to max position \n")
 		servo.max()
 	if pos==-1:
+		print("Turning to min position \n")
 		servo.min()
 
 if __name__ == '__main__':
+	import sys
 	servo = Servo(int(sys.argv[1]))
 	setServoPos(servo, int(sys.argv[2]))
