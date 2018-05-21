9d02995d308d54299659b6037b4702ce568ce7b5
Pi 3 <nhevan@gmail.com>
2018-05-21 23:06:42 +0600

    py-servo.py is ready to be used as alpha release on Rover

--- py-servo.py
+++ py-servo.py
@@ -4,23 +4,24 @@ from gpiozero import Servo
 from time import sleep
 
 def setServoPos(servo, pos):
+	sleep_time = 0.5
 	if pos==0:
 		print("Turning to mid position \n")
 		while True: 
 		    servo.mid()
-		    sleep(1)
+		    sleep(sleep_time)
 		    break
 	if pos==1:
 		print("Turning to max position \n")
 		while True: 
 		    servo.max()
-		    sleep(1)
+		    sleep(sleep_time)
 		    break
 	if pos==-1:
 		print("Turning to min position \n")
 		while True: 
 		    servo.min()
-		    sleep(1)
+		    sleep(sleep_time)
 		    break
 
 if __name__ == '__main__':
